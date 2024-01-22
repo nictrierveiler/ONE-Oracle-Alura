2079a4f319056f191dae51a80bf3b9be2833f7f1
Nicolas Trierveiler <nicolastrierveiler@gmail.com>
2024-01-22 02:57:06 -0300

    Iniciando com JavaScript 01

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do numero secreto!');
+let numeroSecreto = 5;
+let palpite = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log('Isso aí! Você descobriu o numero secreto! (5)');
+};
