b7983a87e8af4c20286cfc52dbd64fc993bb5def
Nicolas Trierveiler <nicolastrierveiler@gmail.com>
2024-01-23 06:23:14 -0300

    WIP challenge02

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do numero secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
+console.log(numeroSecreto);
 let palpite = prompt('Escolha um número entre 1 e 10');
 
+ // se o chute for igual ao numero secreto (numeroSecret0)
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o numero secreto! (5)');
-};
+    alert(`Isso aí! Você descobriu o numero secreto! ${numeroSecreto}`);
+} else {
+   alert('Você errou') 
+}
---
+++ challenge02.js
@@ -0,0 +1,7 @@
+let dia = prompt ('Que dia é hoje?');
+if (dia == "Sábado" || dia == "Domingo" || dia == "sabado" || dia == "domingo" || dia == "sábado") {
+    alert ('Bom final de semana!')
+} else {
+    alert ('Boa Semana!')
+};
+
